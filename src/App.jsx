1e1681bb1bbef893b68722fceb32ca8b69a3fa32
Geoff Jamieson <49350011+UnionPAC@users.noreply.github.com>
2023-07-26 12:07:59 -0400

    fixed spacing on homepage

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <div className="h-screen mt-10 flex flex-col items-center">
       <h1 className="text-3xl font-bold mb-4">Learn React Hooks 🦄</h1>
-      <h2 className="text-xl font-semibold my-2">State Hooks</h2>
+      <h2 className="text-xl font-semibold m-4">State Hooks</h2>
       <ul className="space-y-2 text-center">
         <li>
           <Link to="/useState/Counter">Counter</Link>
@@ -20,32 +20,32 @@ const App = () => {
         </li>
       </ul>
 
-      <h2 className="text-xl font-semibold my-2">Context Hooks</h2>
+      <h2 className="text-xl font-semibold m-4">Context Hooks</h2>
       <ul className="space-y-2 text-center">
         <li></li>
       </ul>
 
-      <h2 className="text-xl font-semibold my-2">Ref Hooks</h2>
+      <h2 className="text-xl font-semibold m-4">Ref Hooks</h2>
       <ul className="space-y-2 text-center">
         <li></li>
       </ul>
 
-      <h2 className="text-xl font-semibold my-2">Effect Hooks</h2>
+      <h2 className="text-xl font-semibold m-4">Effect Hooks</h2>
       <ul className="space-y-2 text-center">
         <li></li>
       </ul>
 
-      <h2 className="text-xl font-semibold my-2">Performance Hooks</h2>
+      <h2 className="text-xl font-semibold m-4">Performance Hooks</h2>
       <ul className="space-y-2 text-center">
         <li></li>
       </ul>
 
-      <h2 className="text-xl font-semibold my-2">Other Hooks</h2>
+      <h2 className="text-xl font-semibold m-4">Other Hooks</h2>
       <ul className="space-y-2 text-center">
         <li></li>
       </ul>
 
-      <h2 className="text-xl font-semibold my-2">Custom Hooks</h2>
+      <h2 className="text-xl font-semibold m-4">Custom Hooks</h2>
       <ul className="space-y-2 text-center">
         <li></li>
       </ul>
